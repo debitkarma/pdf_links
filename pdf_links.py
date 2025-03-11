a25ececf68b5d75c862e7ae9092c7898bd9d7f61
Yatri <debitkarma@gmail.com>
2025-03-10 21:20:30 -0400

    changed link grabbing to work correctly

--- pdf_links.py
+++ pdf_links.py
@@ -10,11 +10,12 @@ def get_all_links_gen(filename: str):
     """Gets all links from a filename that's a PDF"""
     with pymupdf.open(filename, filetype="pdf") as doc:
         for page in doc:
-            link = page.first_link
-            while link:
+            links = page.get_links()
+            for link in links:
                 if link["kind"] == pymupdf.LINK_URI:
-                    yield link
-                link = link.next
+                    yield link["uri"]
+                else:
+                    continue
 
 
 @click.command()
